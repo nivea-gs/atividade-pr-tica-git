6f350977325bafdbff34dc55d039719e21a648ba
nivea-gs <niveaguimelssouza@gmail.com>
2026-01-07 11:25:06 -0300

    adicionando formulário de verificação

---
+++ atv1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício GitHub</title>
+</head>
+<body>
+     <div class="container">
+        <h2>Formulário de Contato</h2>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" placeholder="Digite seu nome">
+            </div>
+          
+            <div class="form-group">
+                <button type="submit">Enviar</button>
+            </div>
+        </form>
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['nome'])){
+    if($_POST['nome'] != ""){
+        echo "Bem-vindo(a), " . $_POST['nome'] . "!";
+    }
+    else{
+        echo "Por favor, informe seu nome!";
+    }
+}
+?>
